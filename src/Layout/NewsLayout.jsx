17b5f9808ac65416b457aedaf4399a1d1e5d9773
Sourav halder <souravh093@gmail.com>
2023-04-24 09:08:16 +0600

    without authentication all done this project

--- src/Layout/NewsLayout.jsx
+++ src/Layout/NewsLayout.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import Header from '../Pages/Shared/Header/Header';
-import { Outlet } from 'react-router-dom';
-import RightSide from '../Pages/Shared/RightSide/RightSide';
-import Footer from '../Pages/Shared/Footer/Footer';
+import React from "react";
+import Header from "../Pages/Shared/Header/Header";
+import { Outlet } from "react-router-dom";
+import RightSide from "../Pages/Shared/RightSide/RightSide";
+import Footer from "../Pages/Shared/Footer/Footer";
 
 const NewsLayout = () => {
-    return (
-        <div>
-            <Header />
-            <div className='grid grid-cols-12 container mx-auto'>
-                <div className='col-span-9 px-10'>
-                    <Outlet />
-                </div>
-                <div className='col-span-3'>
-                    <RightSide />
-                </div>
-            </div>
-            <Footer />
+  return (
+    <div>
+      <Header />
+      <div className="grid grid-cols-12 container mx-auto">
+        <div className="col-span-9 px-10">
+          <Outlet />
         </div>
-    );
+        <div className="col-span-3">
+          <RightSide />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default NewsLayout;
