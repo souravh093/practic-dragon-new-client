61e0c88a919b30e47d53742a2fdfa308560922b0
Sourav halder <souravh093@gmail.com>
2023-04-29 18:37:44 +0600

    some file update

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -4,8 +4,6 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
-console.log('enviroment variable ', import.meta.env.VITE_apiKey)
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
